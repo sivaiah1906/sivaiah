c8bd20c30f635040eae557092055c20cff96b2ea
sivaiah1906 <sivaiahb1906@gmail.com>
2025-08-13 13:15:05 +0530

    Testing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "welcome to applications"
 echo "This is benefits application"
-echo "This is benefits messaging"
-echo "This is benefits contacts"
-echo "This is benefits timeoff"
+echo "This is messaging application"
+echo "This is contacts application"
+echo "This is timeoff application"
