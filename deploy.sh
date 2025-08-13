00c0b60124ecba2cb523144e1bcbefcdfc16edb1
sivaiah1906 <sivaiahb1906@gmail.com>
2025-08-13 12:57:16 +0530

    Create deploy.sh
    Applications

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "welcome to applications"
+echo "This is benefits application"
+echo "This is benefits messaging"
+echo "This is benefits contacts"
+echo "This is benefits timeoff"
